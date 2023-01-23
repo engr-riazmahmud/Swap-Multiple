f6e0f83efa58d7a893d62179bea4e3c5429fd14e
engr-riazmahmud <engineerriazmahmud@gmail.comm>
2023-01-23 19:03:46 +0600

    Swap Two Numbers

--- Swap.cpp
+++ Swap.cpp
@@ -3,26 +3,26 @@ using namespace std;
 
 int main()
 {
-    //how to swap two numbers
-    
+    // How to swap two Numbers
+
     int num1, num2, temp, sum;
 
-    cout << "Enter first number: ";
+    cout << "Enter First Number: ";
     cin >> num1;
-    cout << "Enter second number: ";
+    cout << "Enter second Number: ";
     cin >> num2;
 
+    // swap The Number
     temp = num1;
     num1 = num2;
     num2 = temp;
 
     sum = num1 + num2;
-    cout << "After swapping: " << endl;
-    cout << "First number: " << num1 << endl;
-    cout << "Second number: " << num2 << endl;
-    cout << "Sum of the two numbers: " << sum << endl;
+
+    cout << "After Swapping" << endl;
+    cout << "First Number: " << num1 << endl;
+    cout << "Second Number: " << num2 << endl;
+    cout << "Sum of the two Numbers: " << sum << endl;
 
     return 0;
 }
-
-
